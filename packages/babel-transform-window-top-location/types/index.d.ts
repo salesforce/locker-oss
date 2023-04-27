17e57de881f9fb5c1d60df6753d8c08c7621d193
Alex Chabot <a.chabot@salesforce.com>
2023-04-27 15:06:37 -0400

    fix: remove unused babel
    Signed-off-by: Alex Chabot <a.chabot@salesforce.com>

--- packages/babel-transform-window-top-location/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { NodePath } from '@babel/core';
-import type * as BabelTypesNamespace from '@babel/types';
-export declare function topLocationTransform(): {
-    visitor: {
-        MemberExpression(path: NodePath<BabelTypesNamespace.MemberExpression>): void;
-        AssignmentExpression(path: NodePath<BabelTypesNamespace.AssignmentExpression>): void;
-    };
-};
-//# sourceMappingURL=index.d.ts.map
